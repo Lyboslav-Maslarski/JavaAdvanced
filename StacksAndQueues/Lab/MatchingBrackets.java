d0f95c368deab4343c57a85effec3685f0687167
Lyboslav-Maslarski <lyboslav.maslarski@gmail.com>
2022-08-19 14:34:17 +0300

    add new exercise

--- StacksAndQueues/Lab/MatchingBrackets.java
+++ StacksAndQueues/Lab/MatchingBrackets.java
@@ -1,4 +1,21 @@
 package StacksAndQueues.Lab;
 
+import java.util.ArrayDeque;
+import java.util.Scanner;
+
 public class MatchingBrackets {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String input = scan.nextLine();
+        ArrayDeque<Integer> indexes = new ArrayDeque<>();
+        for (int i = 0; i < input.length(); i++) {
+            char currentChar = input.charAt(i);
+            if (currentChar == '(') {
+                indexes.push(i);
+            } else if (currentChar == ')') {
+                String expression = input.substring(indexes.pop(), i + 1);
+                System.out.println(expression);
+            }
+        }
+    }
 }
