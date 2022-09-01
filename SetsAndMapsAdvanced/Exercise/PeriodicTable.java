4890fb7313acd6b01b96b3dfb599d01e6478d6fc
Lyboslav-Maslarski <lyboslav.maslarski@gmail.com>
2022-09-01 13:47:57 +0300

    add new exercise

---
+++ SetsAndMapsAdvanced/Exercise/PeriodicTable.java
@@ -0,0 +1,22 @@
+package SetsAndMapsAdvanced.Exercise;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class PeriodicTable {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        //4
+        //Ce O
+        //Mo O Ce
+        //Ee
+        //Mo
+        int n = Integer.parseInt(scan.nextLine());
+        Set<String> elements = new TreeSet<>();
+        while (n-- > 0) {
+            List<String> currentElements = Arrays.stream(scan.nextLine().split("\\s+")).collect(Collectors.toList());
+            elements.addAll(currentElements);
+        }
+        System.out.println(String.join(" ", elements));
+    }
+}
