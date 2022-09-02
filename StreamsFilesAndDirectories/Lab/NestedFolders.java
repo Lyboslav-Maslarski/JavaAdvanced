0ce36dd7dad34e716df0172cc863b6b95cc4c624
Lyboslav-Maslarski <lyboslav.maslarski@gmail.com>
2022-09-02 19:02:12 +0300

    add new exercise

--- StreamsFilesAndDirectories/Lab/NestedFolders.java
+++ StreamsFilesAndDirectories/Lab/NestedFolders.java
@@ -4,10 +4,8 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public class NestedFolders {
     public static void main(String[] args) throws IOException {
