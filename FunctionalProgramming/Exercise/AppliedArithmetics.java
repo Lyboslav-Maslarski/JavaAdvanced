8336e8218fda582035b9c02b2e7d021c36d05c39
Lyboslav-Maslarski <lyboslav.maslarski@gmail.com>
2022-09-05 04:26:51 +0300

    add new exercise

--- FunctionalProgramming/Exercise/AppliedArithmetics.java
+++ FunctionalProgramming/Exercise/AppliedArithmetics.java
@@ -4,7 +4,6 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 import java.util.function.Consumer;
-import java.util.function.Function;
 import java.util.function.UnaryOperator;
 import java.util.stream.Collectors;
 
