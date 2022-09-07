18f083a3321143ebd1040519aeab400491a064b1
Lyboslav-Maslarski <lyboslav.maslarski@gmail.com>
2022-09-07 05:26:33 +0300

    add new exercise

---
+++ DefiningClasses/Exercise/CarSalesman/Car.java
@@ -0,0 +1,37 @@
+package DefiningClasses.Exercise.CarSalesman;
+
+public class Car {
+    private String model;
+    private Engine engine;
+    private int weight;
+    private String color;
+
+    public Car(String model, Engine engine) {
+        this.model = model;
+        this.engine = engine;
+        this.weight = 0;
+        this.color = "n/a";
+    }
+
+    public Car(String model, Engine engine, int weight) {
+        this(model, engine);
+        this.weight = weight;
+    }
+
+    public Car(String model, Engine engine, String color) {
+        this(model, engine);
+        this.color = color;
+    }
+
+    public Car(String model, Engine engine, int weight, String color) {
+        this(model, engine);
+        this.weight = weight;
+        this.color = color;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s:%n%s%nWeight: %s%nColor: %s"
+                , this.model, this.engine, this.weight == 0 ? "n/a" : String.valueOf(this.weight), this.color);
+    }
+}
