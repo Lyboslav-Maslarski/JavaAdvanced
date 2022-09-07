b385fb082f0a342a15d9554063afe1f19b73a323
Lyboslav-Maslarski <lyboslav.maslarski@gmail.com>
2022-09-07 06:58:53 +0300

    add new exercise

---
+++ WorkshopCustomDataStructures/MyStack/MyStack.java
@@ -0,0 +1,57 @@
+package WorkshopCustomDataStructures.MyStack;
+
+import java.util.function.Consumer;
+
+public class MyStack {
+    private static class Node {
+        private int element;
+        private Node prev;
+
+        public Node(int element, Node prev) {
+            this.element = element;
+            this.prev = prev;
+        }
+    }
+
+    private Node top;
+    private int size;
+
+    public MyStack() {
+    }
+
+    public void push(int element) {
+        this.top = new Node(element, this.top);
+        this.size++;
+    }
+
+    public int pop() {
+        ensureNotEmpty();
+        int element = this.top.element;
+        this.top = this.top.prev;
+        this.size--;
+        return element;
+    }
+
+    public int peek() {
+        ensureNotEmpty();
+        return this.top.element;
+    }
+
+    public void forEach(Consumer<Integer> consumer) {
+        Node node = this.top;
+        while (node != null) {
+            consumer.accept(node.element);
+            node = node.prev;
+        }
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    private void ensureNotEmpty() {
+        if (this.top == null) {
+            throw new IllegalStateException("Cannot execute on empty stack.");
+        }
+    }
+}
