2136f89567c4141c6c04250940022e8b70b16e9f
Lyboslav-Maslarski <lyboslav.maslarski@gmail.com>
2022-09-08 03:31:05 +0300

    add new exercise

--- WorkshopCustomDataStructures/MyStack.java
+++ WorkshopCustomDataStructures/MyStack.java
@@ -2,43 +2,43 @@ package WorkshopCustomDataStructures;
 
 import java.util.function.Consumer;
 
-public class MyStack {
-    private static class Node {
-        private int element;
-        private Node prev;
+public class MyStack<E> {
+    private static class Node<T> {
+        private final T element;
+        private final Node<T> prev;
 
-        public Node(int element, Node prev) {
+        public Node(T element, Node<T> prev) {
             this.element = element;
             this.prev = prev;
         }
     }
 
-    private Node top;
+    private Node<E> top;
     private int size;
 
     public MyStack() {
     }
 
-    public void push(int element) {
-        this.top = new Node(element, this.top);
+    public void push(E element) {
+        this.top = new Node<>(element, this.top);
         this.size++;
     }
 
-    public int pop() {
+    public E pop() {
         ensureNotEmpty();
-        int element = this.top.element;
+        E element = this.top.element;
         this.top = this.top.prev;
         this.size--;
         return element;
     }
 
-    public int peek() {
+    public E peek() {
         ensureNotEmpty();
         return this.top.element;
     }
 
-    public void forEach(Consumer<Integer> consumer) {
-        Node node = this.top;
+    public void forEach(Consumer<E> consumer) {
+        Node<E> node = this.top;
         while (node != null) {
             consumer.accept(node.element);
             node = node.prev;
