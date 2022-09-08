e0bb993c3c7e90cc16e2532d3a4810124f04d3fb
Lyboslav-Maslarski <lyboslav.maslarski@gmail.com>
2022-09-08 06:39:15 +0300

    add new exercise

---
+++ Generics/Exercise/GenericSwapMethodStrings/Box.java
@@ -0,0 +1,22 @@
+package Generics.Exercise.GenericSwapMethodStrings;
+
+import java.util.List;
+
+public class Box<T> {
+    private final T element;
+
+    public Box(T element) {
+        this.element = element;
+    }
+
+    @Override
+    public String toString() {
+        return element.getClass().getName() + ": " + element;
+    }
+
+    public static <T> void swap(List<T> list, int index1, int index2) {
+        T element1 = list.get(index1);
+        list.set(index1, list.get(index2));
+        list.set(index2, element1);
+    }
+}
