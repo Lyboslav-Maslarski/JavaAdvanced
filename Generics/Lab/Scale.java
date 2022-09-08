868e356efc9fce885313b96b3114fb42a343e867
Lyboslav-Maslarski <lyboslav.maslarski@gmail.com>
2022-09-08 05:29:17 +0300

    add new exercise

---
+++ Generics/Lab/Scale.java
@@ -0,0 +1,20 @@
+package Generics.Lab;
+
+public class Scale<T extends Comparable<T>> {
+    private final T left;
+    private final T right;
+
+    public Scale(T left, T right) {
+        this.left = left;
+        this.right = right;
+    }
+
+    public T getHeavier() {
+        if (left.compareTo(right) > 0) {
+            return left;
+        } else if (right.compareTo(left) > 0) {
+            return right;
+        }
+        return null;
+    }
+}
