61e52356f79d95b9de361f415b34ebfa3e4631ff
Lyboslav-Maslarski <lyboslav.maslarski@gmail.com>
2022-09-10 08:17:47 +0300

    add new exercise

--- IteratorsAndComparators/Exercise/StrategyPattern/Person.java
+++ IteratorsAndComparators/Exercise/StrategyPattern/Person.java
@@ -32,7 +32,7 @@ public class Person {
         @Override
         public int compare(Person p1, Person p2) {
             if (p1.getName().length() == p2.getName().length()) {
-                return Character.compare(p1.getName().charAt(0), p2.getName().charAt(0));
+                return Character.compare(p1.getName().toLowerCase().charAt(0), p2.getName().toLowerCase().charAt(0));
             }
             return Integer.compare(p1.getName().length(), p2.getName().length());
 
