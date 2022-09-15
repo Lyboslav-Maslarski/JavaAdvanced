984b7cc0e25192c38ecb78e7ddedee8f11ff2928
Lyboslav-Maslarski <lyboslav.maslarski@gmail.com>
2022-09-15 18:32:49 +0300

    add new exercise

---
+++ BasicAlgorithms/RecursiveFactorial.java
@@ -0,0 +1,20 @@
+package BasicAlgorithms;
+
+import java.util.Scanner;
+
+public class RecursiveFactorial {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int n = Integer.parseInt(scan.nextLine());
+
+        System.out.println(factorial(n));
+    }
+
+    private static long factorial(int n) {
+        if (n == 1) {
+            return n;
+        }
+        return n * factorial(n - 1);
+    }
+}
